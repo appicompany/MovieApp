b9651887d7bc09462f49e993e4a8483c22027948
Levent <plazo1975@gmail.com>
2022-04-15 11:30:41 +0200

    Add project files.

---
+++ MovieApp.Data/MovieDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data
+{
+    public class MovieDataContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+
+        public MovieDataContext(DbContextOptions<MovieDataContext> options):
+
+            base(options) 
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+
+        
+        public DbSet<Movie> Movies { get; set;}
+        public DbSet<Actor> Actors { get; set;} 
+    }
+}
---
+++ MovieApp/DataSeeder.cs
@@ -0,0 +1,49 @@
+﻿using MovieApp.Data;
+
+namespace MovieApp
+{
+    public static class DataSeeder
+    {
+        public static void Seed(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<MovieDataContext>();
+            context.Database.EnsureCreated();
+            AddMovies(context);
+        }
+
+        private static void AddMovies(MovieDataContext context)
+        {
+            var movies = context.Movies.FirstOrDefault();
+            if (movies != null) return;
+
+            context.Movies.Add(new Movie
+            {
+                Title = "The Shawshank Redemption",
+                Year =  1994,
+                Summary =  "Some text",
+                Actors = new List<Actor> 
+                {
+                new Actor { FullName = "Morgan Freeeman"},
+                new Actor { FullName = "Tim Robbins"},
+                }
+            });
+
+            context.Movies.Add(new Movie
+            {
+                Title = "Matrix",
+                Year = 1999,
+                Summary = "Some text about matrix",
+                Actors = new List<Actor>
+                {
+                new Actor { FullName = "Keanu Reeves"},
+                
+                }
+            });
+
+
+            context.SaveChanges();
+        }
+    }
+
+}
