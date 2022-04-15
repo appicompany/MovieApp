81e0a46e74469df4cc9f6f1c25fe5e91836a4b89
Levent <plazo1975@gmail.com>
2022-04-15 14:31:08 +0200

    test migration

---
+++ Migrations/V1.0_inintial_schema.sql
@@ -0,0 +1,41 @@
+-- Table: public.Actors
+-- DROP TABLE IF EXISTS public."Actors";
+
+CREATE TABLE IF NOT EXISTS public."Actors"
+(
+    "Id" integer NOT NULL DEFAULT nextval('"Actors_Id_seq"'::regclass),
+    "FullName" text COLLATE pg_catalog."default" NOT NULL,
+    "MovieId" integer,
+    CONSTRAINT "PK_Actors" PRIMARY KEY ("Id"),
+    CONSTRAINT "FK_Actors_Movies_MovieId" FOREIGN KEY ("MovieId")
+        REFERENCES public."Movies" ("Id") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Actors"
+-- Index: IX_Actors_MovieId
+-- DROP INDEX IF EXISTS public."IX_Actors_MovieId";
+
+CREATE INDEX IF NOT EXISTS "IX_Actors_MovieId"
+    ON public."Actors" USING btree
+    ("MovieId" ASC NULLS LAST)
+    TABLESPACE pg_default;
+	
+-- Table: public.Movies
+-- DROP TABLE IF EXISTS public."Movies";
+
+CREATE TABLE IF NOT EXISTS public."Movies"
+(
+    "Id" integer NOT NULL DEFAULT nextval('"Movies_Id_seq"'::regclass),
+    "Title" text COLLATE pg_catalog."default" NOT NULL,
+    "Year" integer NOT NULL,
+    "Summary" text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "PK_Movies" PRIMARY KEY ("Id")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Movies"
